d8ca6c7e26a394ca5a1db5f5e24f0cd8fc87262a
Muno Ali <amunoali@gmail.com>
2023-01-19 10:48:10 -0500

    leetcode

---
+++ py/ascending.py
@@ -0,0 +1,16 @@
+# arr = [1,2,3,4,5,6]
+
+# n = len(arr)
+# arr = [(arr[i], i) for i in range(n)]
+
+# def countSetBits(val):
+#     count = 0
+#     while val:
+#         count += val % 2
+#         val = val // 2
+#     return count
+
+# sorted_arr = sorted(arr, key=lambda val: (
+#     countSetBits(val[0]), n-val([1]), reverse= True)
+# sorted_arr = val[0] for val in sorted_arr)    
+# print(sorted_arr)
